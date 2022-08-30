13801ebcd586af7debbb5d9ba01ff7e3276fb4b4
Adam Szewczyk <aszewczyk1990@hotmail.com>
2022-08-30 17:58:44 -0400

    added sprites and music

--- intro.js
+++ intro.js
@@ -1,10 +1,18 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
+let  myMusic = new Audio("./images/dungeon.mp3")
+let moveSound = new Audio('./images/movement.wav')
+let victorySound = new Audio('./images/victory.mp3')
 let button = document.querySelector("#start")
+let tutorial = document.querySelector(".tutorial")
+let arrowKeys = document.querySelector(".arrowKeys")
     button.addEventListener("click",function(){
+        myMusic.play();
         randomRoom()
         button.style.display = 'none'
+        tutorial.style.display = 'block'
+        arrowKeys.style.display = 'block'
     })
 
 const blueAll = new Image()
@@ -21,6 +29,7 @@ const yellowUpLeft = new Image()
 const yellowUpRight = new Image()
 const exitOne = new Image()
 const exitTwo = new Image()
+const knightExplore = new Image()
 
 blueAll.src = "./images/blue all.png"
 blueLeftRight.src = "./images/blue leftRight.png"
@@ -36,10 +45,55 @@ yellowUpLeft.src = "./images/yellow upLeft.png"
 yellowUpRight.src = "./images/yellow upRight.png"
 exitOne.src = "./images/ExitOne.png"
 exitTwo.src = "./images/ExitTwo.png"
+knightExplore.src = './images/knightExplore.png'
 
 let blueCount = 0
 let greenCount = 0
 let yellowCount = 0
+
+let animation
+function animateScript() {
+let position = 95
+const interval = 500 
+animation = setInterval ( () => {
+document.getElementById("image").style.backgroundPosition = 
+`-${position}px 0px`
+if (position < 190)
+{ position = position + 95;}
+else
+{ position = 95; }
+}
+, interval )
+} 
+
+function stopAnimate() {
+    clearInterval(tID);
+    }
+
+class Player {
+    constructor(x,y,w,h) {
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+    }
+    start() {
+        animateScript()
+        let mainChar = document.querySelector("#image")
+        mainChar.style.display = 'block'
+    }
+    explore() {
+        let mainChar = document.querySelector("#image")
+        mainChar.style.display = 'block'
+    }
+    hide() {
+        moveSound.play()
+        let mainChar = document.querySelector("#image")
+        mainChar.style.display = 'none'
+    }
+}
+
+let p1 = new Player(Player.x,Player.y,Player.w,Player.h)
 
 class transition {
     constructor(x,y,w,h) {
@@ -124,19 +178,27 @@ function randomRoom() {
         case 1:
             blueCount ++
             ctx.clearRect(0,0,700,500)
-            // blueAll.onload = function () {
             ctx.drawImage(blueAll,0,0,700,500)
-            // }
+            p1.start()
             document.onkeydown = function() {
                 switch(event.code) {
                     case "ArrowUp":
-                        blueRoomMove()                                                     
+                        blueRoomMove()
+                        p1.hide()
+                        tutorial.style.display = 'none'
+                        arrowKeys.style.display = 'none'                                                     
                         break
                     case "ArrowLeft":
                         blueRoomMove()
+                        p1.hide()
+                        tutorial.style.display = 'none'
+                        arrowKeys.style.display = 'none'
                         break
                     case "ArrowRight":
                         blueRoomMove()
+                        p1.hide()
+                        tutorial.style.display = 'none'
+                        arrowKeys.style.display = 'none'
                         break
                 }
             }
@@ -147,13 +209,20 @@ function randomRoom() {
             // blueLeftRight.onload = function () {
             ctx.drawImage(blueLeftRight,0,0,700,500)
             // }
+            p1.start()
             document.onkeydown = function() {
                 switch(event.code) {
                     case "ArrowLeft":
                         blueRoomMove()
+                        p1.hide()
+                        tutorial.style.display = 'none'
+                        arrowKeys.style.display = 'none'
                         break
                     case "ArrowRight":
                         blueRoomMove()
+                        p1.hide()
+                        tutorial.style.display = 'none'
+                        arrowKeys.style.display = 'none'
                         break
                 }
             }
@@ -164,13 +233,20 @@ function randomRoom() {
             // blueUpLeft.onload = function () {
             ctx.drawImage(blueUpLeft,0,0,700,500)
             // }
+            p1.start()
             document.onkeydown = function() {
                 switch(event.code) {
                     case "ArrowUp":                   
                         blueRoomMove()
+                        p1.hide()
+                        tutorial.style.display = 'none'
+                        arrowKeys.style.display = 'none'
                         break
                     case "ArrowLeft":
                         blueRoomMove()
+                        p1.hide()
+                        tutorial.style.display = 'none'
+                        arrowKeys.style.display = 'none'
                         break
 
                 }
@@ -182,13 +258,20 @@ function randomRoom() {
             // blueUpRight.onload = function () {
             ctx.drawImage(blueUpRight,0,0,700,500)
             // }
+            p1.start()
             document.onkeydown = function() {
                 switch(event.code) {
                     case "ArrowUp":                   
                         blueRoomMove()
+                        p1.hide()
+                        tutorial.style.display = 'none'
+                        arrowKeys.style.display = 'none'
                         break
                     case "ArrowRight":
                         blueRoomMove()
+                        p1.hide()
+                        tutorial.style.display = 'none'
+                        arrowKeys.style.display = 'none'
                         break
                 }
             }
@@ -202,16 +285,20 @@ function randomRoom() {
             greenCount ++
             ctx.clearRect(0,0,700,500)
             ctx.drawImage(greenAll,0,0,700,500)
+            p1.explore()
             document.onkeydown = function() {
                 switch(event.code) {
                     case "ArrowUp":                   
                         greenRoomMove()
+                        p1.hide()
                         break
                     case "ArrowLeft":
                         greenRoomMove()
+                        p1.hide()
                         break
                     case "ArrowRight":
                         greenRoomMove()
+                        p1.hide()
                         break
                 }
             }
@@ -220,13 +307,16 @@ function randomRoom() {
             greenCount++
             ctx.clearRect(0,0,700,500)
             ctx.drawImage(greenLeftRight,0,0,700,500)
+            p1.explore()
             document.onkeydown = function() {
                 switch(event.code) {
                     case "ArrowLeft":
                         greenRoomMove()
+                        p1.hide()
                         break
                     case "ArrowRight":
                         greenRoomMove()
+                        p1.hide()
                         break
                 }
             }
@@ -235,14 +325,16 @@ function randomRoom() {
             greenCount++
             ctx.clearRect(0,0,700,500)
             ctx.drawImage(greenUpLeft,0,0,700,500)
-
+            p1.explore()
             document.onkeydown = function() {
                 switch(event.code) {
                     case "ArrowUp":                   
                         greenRoomMove()
+                        p1.hide()
                         break
                     case "ArrowLeft":
                         greenRoomMove()
+                        p1.hide()
                         break
                 }
             }
@@ -251,13 +343,16 @@ function randomRoom() {
             greenCount++
             ctx.clearRect(0,0,700,500)
             ctx.drawImage(greenUpRight,0,0,700,500)
+            p1.explore()
             document.onkeydown = function() {
                 switch(event.code) {
                     case "ArrowUp":                   
                         greenRoomMove()
+                        p1.hide()
                         break
                     case "ArrowRight":
                         greenRoomMove()
+                        p1.hide()
                         break
                 }
             }
@@ -271,16 +366,20 @@ function randomRoom() {
             yellowCount++
             ctx.clearRect(0,0,700,500)
             ctx.drawImage(yellowAll,0,0,700,500)
+            p1.explore()
             document.onkeydown = function() {
                 switch(event.code) {
                     case "ArrowUp":                   
                         yellowRoomMove()
+                        p1.hide()
                         break
                     case "ArrowLeft":
                         yellowRoomMove()
+                        p1.hide()
                         break
                     case "ArrowRight":
                         yellowRoomMove()
+                        p1.hide()
                         break
                 }
             }
@@ -289,13 +388,16 @@ function randomRoom() {
             yellowCount++
             ctx.clearRect(0,0,700,500)
             ctx.drawImage(yellowLeftRight,0,0,700,500)
+            p1.explore()
             document.onkeydown = function() {
                 switch(event.code) {
                     case "ArrowLeft":
                         yellowRoomMove()
+                        p1.hide()
                         break
                     case "ArrowRight":
                         yellowRoomMove()
+                        p1.hide()
                         break
                 }
             }
@@ -304,13 +406,16 @@ function randomRoom() {
             yellowCount++
             ctx.clearRect(0,0,700,500)
             ctx.drawImage(yellowUpLeft,0,0,700,500)
+            p1.explore()
             document.onkeydown = function() {
                 switch(event.code) {
                     case "ArrowUp":                   
                         yellowRoomMove()
+                        p1.hide()
                         break
                     case "ArrowLeft":
                         yellowRoomMove()
+                        p1.hide()
                         break
                 }
             }
@@ -319,13 +424,16 @@ function randomRoom() {
             yellowCount++
             ctx.clearRect(0,0,700,500)
             ctx.drawImage(yellowUpRight,0,0,700,500)
+            p1.explore()
             document.onkeydown = function() {
                 switch(event.code) {
                     case "ArrowUp":
                         yellowRoomMove()
+                        p1.hide()
                         break
                     case "ArrowRight":
                         yellowRoomMove()
+                        p1.hide()
                         break
                 }
             }
@@ -338,16 +446,20 @@ function randomRoom2() {
             blueCount++
             ctx.clearRect(0,0,700,500)
             ctx.drawImage(blueAll,0,0,700,500)
+            p1.explore()
             document.onkeydown = function() {
                 switch(event.code) {
                     case "ArrowUp":
                         blueRoomMove()
+                        p1.hide()
                         break
                     case "ArrowLeft":
                         blueRoomMove()
+                        p1.hide()
                         break
                     case "ArrowRight":
                         blueRoomMove()
+                        p1.hide()
                         break
                 }
             }
@@ -356,13 +468,16 @@ function randomRoom2() {
             blueCount ++
             ctx.clearRect(0,0,700,500)
             ctx.drawImage(blueLeftRight,0,0,700,500)
+            p1.explore()
             document.onkeydown = function() {
                 switch(event.code) {
                     case "ArrowLeft":
                         blueRoomMove()
+                        p1.hide()
                         break
                     case "ArrowRight":
                         blueRoomMove()
+                        p1.hide()
                         break
                 }
             }
@@ -371,13 +486,16 @@ function randomRoom2() {
             blueCount ++
             ctx.clearRect(0,0,700,500)
             ctx.drawImage(blueUpLeft,0,0,700,500)
+            p1.explore()
             document.onkeydown = function() {
                 switch(event.code) {
                     case "ArrowUp":     
-                        blueRoomMove()              
+                        blueRoomMove()
+                        p1.hide()
                         break
                     case "ArrowLeft":
                         blueRoomMove()
+                        p1.hide()
                         break
                 }
             }
@@ -386,13 +504,16 @@ function randomRoom2() {
             blueCount ++
             ctx.clearRect(0,0,700,500)
             ctx.drawImage(blueUpRight,0,0,700,500)
+            p1.explore()
             document.onkeydown = function() {
                 switch(event.code) {
                     case "ArrowUp":
                         blueRoomMove()                   
+                        p1.hide()
                         break
                     case "ArrowRight":
                         blueRoomMove()
+                        p1.hide()
                         break
                 }
             }
@@ -401,6 +522,8 @@ function randomRoom2() {
     }
 
 function victory() {
+    myMusic.pause()
+    victorySound.play()
     ctx.clearRect(155,150,410,200)
     ctx.fillStyle = 'black'
     ctx.fillRect(155,150, 410, 200)
@@ -419,6 +542,8 @@ function exitRoom() {
         ctx.drawImage(exitOne,0,0,700,500)
         document.onkeydown = function() {
             if(event.code === 'ArrowUp'){
+                let mainChar = document.querySelector("#image")
+                mainChar.style.display = 'none'
                 ctx.clearRect(0,0,700,500)
                     ctx.drawImage(exitTwo,0,0,700,500)
                     setTimeout(() => {  victory(); }, 1000); 
